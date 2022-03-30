c30accd9e28f0e73adcf6df2f565a7543d6a5498
Tsubarah <Tsubarah@hotmail.se>
2022-03-30 18:09:46 +0200

    .

---
+++ 02-vue-basics/app.js
@@ -0,0 +1,69 @@
+/**
+ * Vue.js Basics
+ *
+ */
+
+// Create our Vue app
+const app = Vue.createApp({
+  data() {
+    return {
+      count: 0,
+      msg: 'Hello, Vue!',
+      salary: 10,
+      showBox: true,
+      x: 0,
+      y: 0,
+    }
+  },
+  methods: {
+    getSalaryImg() {
+      return this.salary >= 50
+        ? 'https://c.tenor.com/6Hixx4SFAeQAAAAM/backing-you-get-yours.gif'
+        : 'https://c.tenor.com/noQy6HSBoZ4AAAAd/poor-no-money.gif'
+    },
+    increaseSalary(amount = 1) {
+      this.salary += amount
+    },
+    decreaseSalary(amount = 1) {
+      /**
+       * 3 ways of doing it
+       */
+
+      /*
+      if (this.salary - amount >= 5) {
+        this.salary -= amount
+      } else {
+        this.salary = 5;
+      }
+      */
+
+      /*
+      if (this.salary - amount < 5) {
+       this.salary = 5
+       return
+      }
+      */
+
+     this.salary = (this.salary - amount >= 5)
+        ? this.salary - amount
+        : 5
+    }, 
+
+    updateCoords(e) {
+      this.x = e.offsetX
+      this.y = e.offsetY
+    },
+
+    toggleBox() {
+      this.showBox = !this.showBox
+    }
+  },
+  computed: {
+    salaryClass() {
+      return this.salary >= 20 ? 'good-salary' : 'bad-salary'
+    }
+  }
+})
+
+// Mounts the created app to the div #app in the DOM
+app.mount('#app')
