c0429b24977d18d6bb86358df40da49074561467
Benjamin Tsubarah <benjamin@tsubarah.com>
2022-03-31 12:42:27 +0200

    Completed todo-list

--- vue-todo-app/app.js
+++ vue-todo-app/app.js
@@ -6,25 +6,33 @@ const app = Vue.createApp({
   data() {
     return {
       message: 'Hello Vue!',
-      todo: {
-        title: '',
-        completed: false,
-      },
+      todoTitle: '',
       todos: [
-        { title: 'Go to the gym', completed: true},
+        { title: 'Go to the gym', completed: false},
         { title: 'Clean my desk', completed: false}
       ],
     }
   },
   methods: {
     addTodo() {
-      this.todos.push(this.todo)
-      this.todo = {
-        title: '',
+      this.todos.push({
+        title: this.todoTitle,
         completed: false,
-      }
+      })
+      this.todoTitle = ''
+    },
+    deleteTodo(todo) {
+      this.todos = this.todos.filter(li => li !== todo)
+    },
+    toggleComplete(todo) {
+      todo.completed = !todo.completed
     }
-  }
+  },
+  computed: {
+    unfinishedTodos() {
+      return this.todos.filter(todo => !todo.completed).length
+    }
+  },
 });
 
 app.mount('#app')
