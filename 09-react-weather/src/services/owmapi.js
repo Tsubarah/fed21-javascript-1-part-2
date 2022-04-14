a845dd641736c05c52eac88be42959dd789b556b
Tsubarah <Tsubarah@hotmail.se>
2022-04-14 09:53:22 +0200

    .

--- 09-react-weather/src/services/owmapi.js
+++ 09-react-weather/src/services/owmapi.js
@@ -2,11 +2,25 @@
  * Open Weather Map API
  */
 
+import axios from 'axios'
+
 const API_KEY = '32cd3c29aab67ddbedd8d8db1d91d627'
 const BASE_URL = "https://api.openweathermap.org/data/2.5"
 
+
 /**
- * Get current weather for a city.
+ * Get current weather for a city with Axios. With Axios we dont need to convert from JSON. It needs to be in a try/catch though.
+ *
+ * @param {string} query City to get current weather for
+ */
+
+const getCurrentWeatherWithAxios = async query => {
+	const response = await axios.get(`${BASE_URL}/weather?q=${query}&units=metric&appid=${API_KEY}`)
+}
+
+
+/**
+ * Get current weather for a city with fetch.
  *
  * @param {string} query City to get current weather for
  */
