391e3b7e4f1bb456bb1078beeac6bee2479337fc
Benjamin Tsubarah <benjamin@tsubarah.com>
2022-04-27 11:05:58 +0200

    Created ThemeContextProvider

---
+++ 12-yesAPI/src/App.jsx
@@ -0,0 +1,30 @@
+import { useContext } from 'react'
+import Container from 'react-bootstrap/Container'
+import { Routes, Route } from 'react-router-dom'
+import Navigation from './components/Navigation'
+import { ThemeContext } from './contexts/ThemeContextProvider'
+import HomePage from './pages/HomePage'
+import SearchHackerNews from './pages/SearchHackerNews'
+import NotFound from './pages/NotFound'
+import 'bootstrap/dist/css/bootstrap.css'
+import './App.css'
+
+const App = () => {
+	const { isDarkTheme } = useContext(ThemeContext)
+
+	return (
+		<div id="App" className={isDarkTheme() ? 'bg-dark text-success' : ''}>
+			<Navigation />
+
+			<Container className="py-3">
+				<Routes>
+					<Route path="/" element={<HomePage />} />
+					<Route path="/search" element={<SearchHackerNews />} />
+					<Route path="*" element={<NotFound />} />
+				</Routes>
+			</Container>
+		</div>
+	)
+}
+
+export default App
