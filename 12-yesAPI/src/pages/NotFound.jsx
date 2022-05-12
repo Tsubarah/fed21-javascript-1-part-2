0b6cac5bd3930c46a5e4c6759686ea8e72e7d0f4
Benjamin Tsubarah <benjamin@tsubarah.com>
2022-05-12 11:14:44 +0200

    added mounting to yesAPI

--- 12-yesAPI/src/pages/NotFound.jsx
+++ 12-yesAPI/src/pages/NotFound.jsx
@@ -1,4 +1,21 @@
+import { useState, useEffect} from 'react'
+
+
 const NotFound = () => {
+
+const [mount, setMount] = useState()
+
+const onMount = () => {
+
+	console.log("I am mounted")
+	
+	}
+
+useEffect (() => {
+
+setMount(onMount)
+
+}, [] ) 
 	return (
 		<>
 			<h1>Sorry, that page could not be found 😔</h1>
